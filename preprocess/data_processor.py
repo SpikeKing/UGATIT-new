04a82383653c645047a413a10bfb602047ccab7f
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-02-11 20:30:48 +0800

    修改变量值

--- preprocess/data_processor.py
+++ preprocess/data_processor.py
@@ -15,12 +15,15 @@ from utils.project_utils import traverse_dir_files, mkdir_if_not_exist
 
 class DataProcessor(object):
     def __init__(self):
-        self.cartoons_path = os.path.join(ROOT_DIR, 'dataset', 'cartoons')
-        self.persons_path = os.path.join(ROOT_DIR, 'dataset', 'persons')
+        self.cartoons_path = os.path.join(ROOT_DIR, 'dataset', 'anime_face')
+        self.persons_path = os.path.join(ROOT_DIR, 'dataset', 'chs_stars_512px')
 
         self.std_dim = 256
 
     def process_dataset(self):
+        """
+        处理数据集
+        """
         c_paths_list, c_names_list = traverse_dir_files(self.cartoons_path)
         p_paths_list, p_names_list = traverse_dir_files(self.persons_path)
 
@@ -28,13 +31,13 @@ class DataProcessor(object):
         random.shuffle(c_paths_list)
         random.shuffle(p_paths_list)
 
-        train_size = 10
-        test_size = 5
-        print_size = 5
+        train_size = 80000  # 训练集量
+        test_size = 5000  # 测试集量
+        print_size = 1000
 
         count = 0
-        train_person_dir = os.path.join(ROOT_DIR, 'dataset', 'zhengsheng_s2a', 'trainA')
-        test_person_dir = os.path.join(ROOT_DIR, 'dataset', 'zhengsheng_s2a', 'testA')
+        train_person_dir = os.path.join(ROOT_DIR, 'dataset', 's2a_zhengsheng', 'trainA')
+        test_person_dir = os.path.join(ROOT_DIR, 'dataset', 's2a_zhengsheng', 'testA')
         mkdir_if_not_exist(train_person_dir)
         mkdir_if_not_exist(test_person_dir)
 
@@ -62,8 +65,8 @@ class DataProcessor(object):
                 break
 
         count = 0
-        train_cartoon_dir = os.path.join(ROOT_DIR, 'dataset', 'zhengsheng_s2a', 'trainB')
-        test_cartoon_dir = os.path.join(ROOT_DIR, 'dataset', 'zhengsheng_s2a', 'testB')
+        train_cartoon_dir = os.path.join(ROOT_DIR, 'dataset', 's2a_zhengsheng', 'trainB')
+        test_cartoon_dir = os.path.join(ROOT_DIR, 'dataset', 's2a_zhengsheng', 'testB')
         mkdir_if_not_exist(train_cartoon_dir)
         mkdir_if_not_exist(test_cartoon_dir)
 
